ab23d6eec214cb1f2bcff0564ca036809279589f
caiyiheng <rangercyh@qq.com>
2014-09-17 14:57:30 +0800

    Update lzw.lua

--- lzw.lua
+++ lzw.lua
@@ -1,10 +1,17 @@
-local szSource = "ABABABABBBABABAACDACDADCABAAABAB"
+local szSource = "AAABBBCCCDDD"
 local nBeginCode = 6
+local tbChar = {
+	["A"] = true,
+	["B"] = true,
+	["C"] = true,
+	["D"] = true,
+}
+
 function lzwCode(szSource)
 	local tbOutput = {}
 	local szPrefix = ""
 	local tbToken = {}
-	local nTokenNum = nBeginCode
+	local nTokenCode = nBeginCode
 	for szChar in string.gmatch(szSource, "%a") do
 		if szPrefix == "" then
 			szPrefix = szChar
@@ -12,33 +19,77 @@ function lzwCode(szSource)
 			if tbToken[szPrefix .. szChar] then
 				szPrefix = tbToken[szPrefix .. szChar]
 			else
-				tbToken[szPrefix .. szChar] = nTokenNum
-				nTokenNum = nTokenNum + 1
+				tbToken[szPrefix .. szChar] = nTokenCode
+				nTokenCode = nTokenCode + 1
 				table.insert(tbOutput, szPrefix)
 				szPrefix = szChar
 			end
 		end
 	end
+	table.insert(tbOutput, szPrefix)
 	return tbOutput
 end
 
-print(szSource)
-print(table.concat(lzwCode(szSource), ","))
+function lzwDecodeToSource(szSource, tbDeCodeToken, szPrefix)
+	local tbPrefix = { szPrefix }
+	while #tbPrefix ~= 0 do
+		local szFirst = table.remove(tbPrefix, 1)
+		local tbDecode = tbDeCodeToken[tonumber(szFirst)]
+		if tbDecode then
+			table.insert(tbPrefix, 1, tbDecode[2])
+			table.insert(tbPrefix, 1, tbDecode[1])
+		else
+			szSource = szSource .. szFirst
+		end
+	end
+	return szSource
+end
 
-function lzwDecode(tbCode)
+function lzwDecode(tbCode, tbChar)
 	local szSource = ""
 	local szPrefix = ""
 	local tbToken = {}
+	local tbDeCodeToken = {}
+	local nTokenCode = nBeginCode
 	for _, szCode in ipairs(tbCode) do
-		if szPrefix == "" then
-			szPrefix = szCode
-		else
-			if tbToken[szPrefix .. szCode] then
-			
+		if szPrefix ~= "" then
+			if tbChar[szCode] then	--判断后缀是否是正常后缀
+				tbToken[szPrefix .. szCode] = nTokenCode
+				tbDeCodeToken[nTokenCode] = { szPrefix, szCode }
+				nTokenCode = nTokenCode + 1
+				--前缀放入输出流
+				szSource = lzwDecodeToSource(szSource, tbDeCodeToken, szPrefix)
 			else
-				
+				if tbDeCodeToken[tonumber(szCode)] then
+					tbToken[szPrefix .. tbDeCodeToken[tonumber(szCode)][1]] = nTokenCode
+					tbDeCodeToken[nTokenCode] = { szPrefix, tbDeCodeToken[tonumber(szCode)][1] }
+					nTokenCode = nTokenCode + 1
+					--前缀放入输出流
+					szSource = lzwDecodeToSource(szSource, tbDeCodeToken, szPrefix)
+				else
+					if tbChar[szPrefix] then
+						tbToken[szPrefix .. szPrefix] = nTokenCode
+						tbDeCodeToken[nTokenCode] = { szPrefix, szPrefix }
+					else
+						tbToken[szPrefix .. tbDeCodeToken[tonumber(szPrefix)][1]] = nTokenCode
+						tbDeCodeToken[nTokenCode] = { szPrefix, tbDeCodeToken[tonumber(szPrefix)][1] }
+					end
+					nTokenCode = nTokenCode + 1
+					--前缀放入输出流
+					szSource = lzwDecodeToSource(szSource, tbDeCodeToken, szPrefix)
+				end
 			end
 		end
+		szPrefix = szCode
 	end
+	szSource = lzwDecodeToSource(szSource, tbDeCodeToken, szPrefix)
+	return szSource
 end
+
+print(szSource)
+local tbCode = lzwCode(szSource)
+print(table.concat(tbCode, ","))
+print(lzwDecode(tbCode, tbChar))
+
+
 
